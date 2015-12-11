d10a956ab6f2b07b96639f41adf6eca03c031fcd
Kang-min Liu <gugod@gugod.org>
2015-12-11 23:51:34 +0100

    support format suffix

--- server.pl
+++ server.pl
@@ -3,18 +3,49 @@ use strict;
 use warnings;
 use Time::HiRes qw(sleep time);
 use Plack::Request;
+use Plack::Response;
+use JSON;
+use YAML;
+
+sub build_response {
+    my ($res_data) = @_;
+    my $res = Plack::Response->new(200);
+    if ($res_data->{format}) {
+        if ($res_data->{format} eq 'json') {
+            $res->content_type("application/json");
+            $res->body("". JSON->new->utf8->encode($res_data));
+        }
+        elsif ($res_data->{format} =~ /\A ya?ml \z/x) {
+            $res->content_type("text/yaml");
+            $res->body("". JSON->new->utf8->encode($res_data));
+        }
+    } else {
+        $res->content_type("text-html");
+        my $res_body = "<html><head><meta charset=\"utf8\"></head><body>"
+        . join( "<br>\n",
+                (map { ($_, $res_data->{$_} //"") } sort keys %$res_data)
+           ) ."</body></html>";
+        $res->body($res_body);
+    }
+    return $res->finalize;
+}
 
 sub {
     my $env = shift;
     my $start_time = time;
     my $req = Plack::Request->new($env);
+    my $res_data = {};
 
     if (my $t = $req->query_parameters->{'it-takes-time'}) {
         if ($t =~ /\A( [1-9][0-9]* | [0-9]\.[0-9]+ )\z/x) {
             sleep $t;
+            $res_data->{start_time} = $start_time;
+            $res_data->{end_time}   = time;
         }
     }
-    return [200, [], [$start_time, ",", time]];
+    ($res_data->{format}) = $req->path_info =~ m/\.(json|ya?ml)\z/i and $res_data->{format} = lc($res_data->{format});
+
+    return build_response($res_data);
 }
 
 __END__
