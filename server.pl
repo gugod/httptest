10ba4fcca7cc6058a5e2bc983f74f1a6cfbefb68
Kang-min Liu <gugod@gugod.org>
2015-12-14 20:17:14 +0100

    Add a small path-based dispatcher here.

--- server.pl
+++ server.pl
@@ -34,12 +34,17 @@ sub build_response {
 }
 
 sub dispatch_and_fleshen_res_data {
-    my ($res_data, $req, $env) = @_;
-    state $delimitor = "[/.?]";
-    for($req->path_info) {
-        (m!\A /dumpenv $delimitor !x) and do {
-            $res_data->{env} = $env;
-        };
+    my ($tx) = @_;
+
+    state $action = {
+        "/dumpenv" => sub {
+            my ($tx) = @_;
+            $tx->{res_data}{body}{env} = [keys %{$tx->{env}}];
+        }
+    };
+
+    if (my $act = $action->{$tx->{res_data}{path}}) {
+        $act->($tx);
     }
 }
 
@@ -49,6 +54,16 @@ sub {
     my $req = Plack::Request->new($env);
     my $res_data = { header => {}, body => {}, format => undef };
 
+    my ($path, $format) = split(qr{[.?]}, $req->path_info);
+
+    if (defined($format)) {
+        $format = lc($format);
+    }
+    $res_data->{format} = $format;
+    $res_data->{path}   = $path;
+
+    dispatch_and_fleshen_res_data({ res_data => $res_data, req => $req, env => $env });
+
     if (my $t = $req->query_parameters->{'it-takes-time'}) {
         if ($t =~ /\A( [1-9][0-9]* | [0-9]\.[0-9]+ )\z/x) {
             sleep $t;
@@ -56,10 +71,6 @@ sub {
             $res_data->{body}{end_time}   = time;
         }
     }
-
-    dispatch_and_fleshen_res_data($res_data, $req, $env);
-
-    ($res_data->{format}) = $req->path_info =~ m/\.(json|ya?ml)\z/i and $res_data->{format} = lc($res_data->{format});
 
     return build_response($res_data);
 }
